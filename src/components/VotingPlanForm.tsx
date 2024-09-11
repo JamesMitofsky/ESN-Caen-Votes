1e05e0efee9df869f3ad0f1a86f9c6ab271e5502
James Mitofsky <JamesMitofsky@gmail.com>
2024-09-11 13:33:38 +0200

    feat(spacing): add spacing between the form and the bottom of the page

--- src/components/VotingPlanForm.tsx
+++ src/components/VotingPlanForm.tsx
@@ -5,7 +5,7 @@ export const VotingPlanForm = () => {
     <form
       action='https://api.web3forms.com/submit'
       method='POST'
-      className='w-full max-w-sm'
+      className='w-full max-w-sm mb-6'
     >
       <input
         type='hidden'
@@ -13,15 +13,6 @@ export const VotingPlanForm = () => {
         value='b580566a-512b-4451-a837-79841efd92c6'
       />
       <div className='md:flex md:items-center mb-6'>
-        {/* <div className='md:w-1/3'>
-          <label
-            className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
-            htmlFor='name'
-          >
-            Name
-          </label>
-        </div> */}
-        {/* <div className='md:w-2/3'> */}
         <input
           className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
           id='name'
@@ -30,18 +21,8 @@ export const VotingPlanForm = () => {
           placeholder='Name'
           required
         />
-        {/* </div> */}
       </div>
       <div className='md:flex md:items-center mb-6'>
-        {/* <div className='md:w-1/3'>
-          <label
-            className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
-            htmlFor='email'
-          >
-            Email
-          </label>
-        </div> */}
-        {/* <div className='md:w-2/3'> */}
         <input
           className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
           id='email'
@@ -50,41 +31,19 @@ export const VotingPlanForm = () => {
           placeholder='Email'
           required
         />
-        {/* </div> */}
       </div>
-      {/* <div className='md:flex md:items-center mb-6'>
-        <div className='md:w-1/3'>
-          <label
-            className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
-            htmlFor='message'
-          >
-            Message
-          </label>
-        </div>
-        <div className='md:w-2/3'>
-          <textarea
-            className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
-            id='message'
-            name='message'
-            required
-          ></textarea>
-        </div>
-      </div> */}
       <input
         type='hidden'
         name='redirect'
         value='https://web3forms.com/success'
       />
       <div className='flex justify-center'>
-        {/* <div className='md:w-1/3'></div> */}
-        {/* <div className='md:w-2/3'> */}
         <button
           className='shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded'
           type='submit'
         >
           Make a plan to vote!
         </button>
-        {/* </div> */}
       </div>
     </form>
   );
